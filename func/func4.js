fa195fc1bbf788f48aa3600943d8276f937ee869
apple <apple@appleui-MacBook-Pro-4.local>
2018-11-23 03:00:13 +0900

    func4 improved

--- func/func4.js
+++ func/func4.js
@@ -2,6 +2,19 @@ function func4(dictGiven,flag){
   //탭 두번을 기준으로 각 건물들을 나눈 행렬
 
   for(var inx in dictGiven){
+    var thisBld = dictGiven[inx];
+    var bldInf = new RegExp(thisBld[1]+"\\("+thisBld[0]+"\\)","g");
+    var bldInfArray = article.match(bldInf);
+    if(bldInfArray==null){
+      if(flag!=1){
+        var onlyBldNm = new RegExp(thisBld[1],"g");
+        updateOutput(article,onlyBldNm,thisBld[1]+"의 건물 번호는 "+thisBld[0].split("동")[0]+"입니다.","purple");
+      }
+    }
+  }
+  var needFunc4 = article.match(/[0-9]동/);
+  if(needFunc4 != null)
+  for(var inx in dictGiven){
     //금칙어 행렬에서 건물 번호와 건물 이름 행렬로 받아오기
     var thisBld = dictGiven[inx];
 
@@ -37,18 +50,15 @@ function func4(dictGiven,flag){
         }
       }
     }
-    //건물 이름(건물 번호를 한 번도 표기 안 함)
+    //건물 이름(건물 번호)를 한 번도 표기 안 함
     else{
-      if(flag!=1){
-        var onlyBldNm = new RegExp(thisBld[1],"g");
-        updateOutput(article,onlyBldNm,thisBld[1]+"의 건물 번호는 "+thisBld[0].split("동")[0]+"입니다.","purple");
-      }
       var onlyBldNo = LbRegExp("(?<![0-9])"+thisBld[0]);
       updateOutput2(article,onlyBldNo,thisBld[0]+"의 건물 이름은 "+thisBld[1]+"입니다.","purple");
     }
 
-    var bldNm = new RegExp(thisBld[1],"g");
 
   }
+
+
 
 }
